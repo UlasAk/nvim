06c50b2561193f3634188a7ce76fc139c7891daf
Noah Bauer <bauer.noah712@gmx.de>
2024-05-05 03:44:56 +0200

    add telescope config and noice config

---
+++ lua/configs/noice.lua
@@ -0,0 +1,22 @@
+local options = {
+  cmdline = {
+    enabled = true,
+  },
+  notify = {
+    enabled = true,
+  },
+  popupmenu = {
+    enabled = true,
+  },
+  lsp = {
+    hover = {
+      enabled = false,
+    },
+    signature = {
+      enabled = false,
+    },
+  },
+  routes = {},
+}
+
+return options
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -131,22 +131,35 @@ return {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {},
     dependencies = {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
     },
     config = function()
-      require("noice").setup {
-        lsp = {
-          hover = {
-            enabled = false,
-          },
-          signature = {
-            enabled = false,
-          },
-        },
-      }
+      require("noice").setup(require "configs.noice")
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = "Telescope",
+    opts = function()
+      return require "configs.telescope"
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "telescope")
+      local telescope = require "telescope"
+      telescope.setup(opts)
+      -- load extensions
+      for _, ext in ipairs(opts.extensions_list) do
+        telescope.load_extension(ext)
+      end
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").load_extension "ui-select"
     end,
   },
   {
