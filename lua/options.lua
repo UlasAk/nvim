bd731676b007aeb95b17fe9d98f362df6c438402
Noah Bauer <bauer.noah712@gmx.de>
2024-05-05 04:00:41 +0200

    set current search item to red background

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,11 @@
 require "nvchad.options"
 
--- Syntax Highlighting
+-- Syntax Highlighting for .conf files
 vim.cmd [[
   au BufEnter,BufRead *.conf setf dosini
+]]
+
+-- Highlight current search item with different color than other search items
+vim.cmd [[
+  hi CurSearch guifg=#282737 guibg=#ff0000 
 ]]
