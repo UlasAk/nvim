c9e0ac4c984a22148dd78d2b2e2b561a441fd9d8
Noah Bauer <bauer.noah712@gmx.de>
2024-05-08 13:03:23 +0200

    move nvchad config to local config

--- init.lua
+++ init.lua
@@ -3,44 +3,50 @@ vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
 if not vim.loop.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
+-- load options
+require "options"
 
 -- load plugins
 require("lazy").setup({
+  -- {
+  --   "NvChad/NvChad",
+  --   lazy = false,
+  --   branch = "v2.5",
+  --   import = "nvchad.plugins",
+  --   config = function()
+  --     require "options"
+  --   end,
+  -- },
   {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
+    import = "plugins",
     config = function()
       require "options"
     end,
   },
-
-  { import = "plugins" },
-}, lazy_config)
+}, require "configs.lazy")
 
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
-require "nvchad.autocmds"
+-- load autocmds
+require "autocmds"
 
+-- load key bindings
 vim.schedule(function()
   require "mappings"
 end)
 
+-- vim window options
 vim.opt.colorcolumn = "160"
 vim.g.nvim_terminal_default_window_settings = {
   window = {
-    width = 80
-  }
+    width = 80,
+  },
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,22 @@
-local cmp = require "cmp"
-
 return {
+  "nvim-lua/plenary.nvim",
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     ft = { "lua", "javascript", "typescript" },
     config = function()
       require "configs.conform"
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "User FilePost",
+    opts = function()
+      return require "configs.gitsigns"
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "git")
+      require("gitsigns").setup(opts)
     end,
   },
   {
@@ -23,8 +33,16 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
     opts = function()
       return require "configs.treesitter"
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "syntax")
+      dofile(vim.g.base46_cache .. "treesitter")
+      require("nvim-treesitter.configs").setup(opts)
     end,
   },
   {
@@ -47,6 +65,7 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    event = "User FilePost",
     config = function()
       local lspconfig = require "configs.lsp"
       lspconfig.defaults()
@@ -54,14 +73,23 @@ return {
   },
   {
     "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "rust-analyzer",
-        "prettier",
-        "stylua",
-        "typescript-language-server",
-      },
-    },
+    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+    opts = function()
+      return require "configs.mason"
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "mason")
+      require("mason").setup(opts)
+
+      -- custom nvchad cmd to install all mason binaries listed
+      vim.api.nvim_create_user_command("MasonInstallAll", function()
+        if opts.ensure_installed and #opts.ensure_installed > 0 then
+          vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+        end
+      end, {})
+
+      vim.g.mason_binaries_list = opts.ensure_installed
+    end,
   },
   {
     "rust-lang/rust.vim",
@@ -115,9 +143,48 @@ return {
   },
   {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      {
+        -- snippet plugin
+        "L3MON4D3/LuaSnip",
+        dependencies = "rafamadriz/friendly-snippets",
+        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+        config = function(_, opts)
+          require("luasnip").config.set_config(opts)
+          require "configs.luasnip"
+        end,
+      },
+
+      -- autopairing of (){}[] etc
+      {
+        "windwp/nvim-autopairs",
+        opts = {
+          fast_wrap = {},
+          disable_filetype = { "TelescopePrompt", "vim" },
+        },
+        config = function(_, opts)
+          require("nvim-autopairs").setup(opts)
+
+          -- setup cmp for autopairs
+          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+      },
+
+      -- cmp sources plugins
+      {
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+      },
+    },
     opts = function()
-      local M = require "nvchad.configs.cmp"
+      local M = require "configs.cmp"
       M.completion.completeopt = "menu,menuone,noselect"
+      local cmp = require "cmp"
       M.mapping["<CR>"] = cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Insert,
         select = false,
@@ -125,6 +192,23 @@ return {
       -- add sources to cmp
       table.insert(M.sources, { name = "crates" })
       return M
+    end,
+    config = function(_, opts)
+      require("cmp").setup(opts)
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    keys = {
+      { "gcc", mode = "n", desc = "comment toggle current line" },
+      { "gc", mode = { "n", "o" }, desc = "comment toggle linewise" },
+      { "gc", mode = "x", desc = "comment toggle linewise (visual)" },
+      { "gbc", mode = "n", desc = "comment toggle current block" },
+      { "gb", mode = { "n", "o" }, desc = "comment toggle blockwise" },
+      { "gb", mode = "x", desc = "comment toggle blockwise (visual)" },
+    },
+    config = function(_, opts)
+      require("Comment").setup(opts)
     end,
   },
   {
