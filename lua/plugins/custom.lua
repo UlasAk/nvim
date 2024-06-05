4e8f45f2f08996b776e2bc87b523e45f1cb4a824
Noah Bauer <accounts@noahbauer.de>
2024-06-05 15:26:07 +0200

    load todo comments on file open

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -323,6 +323,7 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    event = { "BufReadPost", "BufNewFile" },
     cmd = "TodoTelescope",
     config = function()
       require("todo-comments").setup()
