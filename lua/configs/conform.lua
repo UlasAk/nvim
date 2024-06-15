168a7363318669718bcedd4da01f48aba8fa1c31
Noah Bauer <accounts@noahbauer.de>
2024-06-15 18:28:23 +0200

    add angular treesitter, but commented out

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,5 +1,6 @@
 local options = {
   formatters_by_ft = {
+    angular = { "prettier" },
     html = { "prettier" },
     lua = { "stylua" },
     javascript = { "prettier" },
--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,5 +1,6 @@
 local options = {
   ensure_installed = {
+    -- "angular",
     "bash",
     "css",
     "javascript",
@@ -29,5 +30,19 @@ local options = {
 
   indent = { enable = true },
 }
+
+-- Angular Setup
+-- vim.filetype.add {
+--   pattern = {
+--     [".*%.component%.html"] = "angular.html", -- Sets the filetype to `angular.html` if it matches the pattern
+--   },
+-- }
+--
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "angular",
+--   callback = function()
+--     vim.treesitter.language.register("angular", "angular.html") -- Register the filetype with treesitter for the `angular` language/parser
+--   end,
+-- })
 
 return options
