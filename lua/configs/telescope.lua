49cc7586856852cfaa78370d849c426632364aac
Noah Bauer <accounts@noahbauer.de>
2024-06-22 03:41:17 +0200

    add telescope live_grep search also for hidden files

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,6 +1,17 @@
 local trouble = require "trouble.sources.telescope"
 
 local options = {
+  pickers = {
+    grep_string = {
+      additional_args = { "--no-ignore", "--hidden" },
+    },
+    live_grep = {
+      additional_args = { "--no-ignore", "--hidden" },
+    },
+    find_file = {
+      hidden = false,
+    },
+  },
   defaults = {
     vimgrep_arguments = {
       "rg",
@@ -33,7 +44,7 @@ local options = {
       preview_cutoff = 120,
     },
     file_sorter = require("telescope.sorters").get_fuzzy_file,
-    file_ignore_patterns = { "node_modules" },
+    -- file_ignore_patterns = { "node_modules" },
     generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
     path_display = { "truncate" },
     winblend = 0,
