7e9332d445704435a48f6adf7d0e1b1f30d0fec1
Noah Bauer <accounts@noahbauer.de>
2024-06-22 04:11:59 +0200

    fix treesitter angular, hyprlang and dosini and tmux config

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -34,27 +34,46 @@ local options = {
 }
 
 -- Add Custom Filetypes
+local function is_hypr_conf(path)
+  return path:match "/hypr/" and path:match "%.conf$"
+end
+
+local function is_tmux_conf(path)
+  return path:match "%tmux.conf$"
+end
+
 vim.filetype.add {
   pattern = {
     -- [".*%.component%.html"] = "angular.html", -- Sets the filetype to `angular.html` if it matches the pattern
-    [".*%.conf"] = "dosini",
-    [".*%/hypr/.*%.conf"] = "hyprlang",
+    [".*%.conf"] = function(path, _)
+      if is_hypr_conf(path) then
+        return "hyprlang"
+      elseif is_tmux_conf(path) then
+        return "tmux"
+      else
+        return "dosini"
+      end
+    end,
   },
 }
 
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+-- Angular
+local function is_angular_template(path)
+  return path:match "%.component%.html$"
+end
+vim.api.nvim_create_augroup("AngularTemplates", {})
+vim.api.nvim_create_autocmd({ "BufRead", "BufEnter", "BufNewFile" }, {
   pattern = "*.component.html",
-  command = "set filetype=angular",
-})
+  callback = function()
+    -- Setze den Dateityp auf HTML, damit HTML-Plugins funktionieren
+    vim.bo.filetype = "html"
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
-  pattern = "hyprlang",
-  command = "set filetype=hyprlang",
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
-  pattern = "dosini",
-  command = "set filetype=dosini",
+    -- Speziell für Treesitter auf Angular setzen
+    if is_angular_template(vim.fn.expand "<afile>:p") then
+      vim.cmd "set filetype=angular"
+    end
+  end,
+  group = "AngularTemplates",
 })
 
 return options
