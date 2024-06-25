5e1e58073be0c259a58ee097b8a2f6a11feaf745
Noah Bauer <accounts@noahbauer.de>
2024-06-25 22:09:28 +0200

    remove unnecessary nvconfig.lua and move contents to chadrc.lua

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,117 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "catppuccin",
+  ------------------------------- base46 -------------------------------------
+  -- hl = highlights
+  hl_add = {},
+  hl_override = {},
+  changed_themes = {},
+  theme_toggle = { "catppuccin", "everforest_light" },
+  theme = "catppuccin", -- default theme
+  transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "atom_colored", -- default/flat_light/flat_dark/atom/atom_colored
+  },
+
+  telescope = { style = "bordered" }, -- borderless / bordered
+
+  ------------------------------- nvchad_ui modules -----------------------------
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "round",
+    order = nil,
+    modules = {
+      cursor = function()
+        return gen_block("", "%l/%c", "%#St_Pos_sep#", "%#St_Pos_bg#", "%#St_Pos_txt#")
+      end,
+    },
+  },
+
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    enabled = true,
+    lazyload = true,
+    order = { "treeOffset", "buffers", "tabs", "btns" },
+    modules = nil,
+  },
+
+  nvdash = {
+    load_on_startup = true,
+
+    header = {
+      "           ▄ ▄                   ",
+      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+
+  cheatsheet = {
+    theme = "grid", -- simple/grid
+    excluded_groups = {}, -- can add group name or with mode
+  },
+
+  lsp = { signature = true },
+
+  term = {
+    hl = "Normal:term,WinSeparator:WinSeparator",
+    sizes = { sp = 0.3, vsp = 0.2 },
+    float = {
+      relative = "editor",
+      row = 0.3,
+      col = 0.25,
+      width = 0.5,
+      height = 0.4,
+      border = "single",
+    },
+  },
+}
+
+local config = M.ui.statusline
+local sep_style = config.separator_style
+
+sep_style = (sep_style ~= "round" and sep_style ~= "block") and "block" or sep_style
+
+local default_sep_icons = {
+  round = { left = "", right = "" },
+  block = { left = "█", right = "█" },
+}
+
+local separators = (type(sep_style) == "table" and sep_style) or default_sep_icons[sep_style]
+
+local sep_l = separators["left"]
+
+M.ui.statusline.modules = {
+  cursor = function()
+    return "%#St_pos_sep#" .. sep_l .. "%#St_pos_icon# %#St_pos_text# %l/%c %p%% "
+  end,
+}
+
+M.base46 = {
+  integrations = {},
 }
 
 return M
