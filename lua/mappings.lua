b40f8e013d8ced3f3acee9f54f8b21e0a28915a0
Noah Bauer <accounts@noahbauer.de>
2024-10-23 14:06:17 +0200

    [ADD] delete function in harpoon list

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -319,6 +319,18 @@ map("n", "<leader>fha", function()
       table.insert(file_paths, item.value)
     end
 
+    local make_finder = function()
+      local paths = {}
+
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(paths, item.value)
+      end
+
+      return require("telescope.finders").new_table {
+        results = paths,
+      }
+    end
+
     require("telescope.pickers")
       .new({}, {
         prompt_title = "Harpoon",
@@ -327,6 +339,20 @@ map("n", "<leader>fha", function()
         },
         previewer = conf.file_previewer {},
         sorter = conf.generic_sorter {},
+        attach_mappings = function(prompt_buffer_number, local_map)
+          -- The keymap you need
+          local_map("i", "<c-d>", function()
+            local state = require "telescope.actions.state"
+            local selected_entry = state.get_selected_entry()
+            local current_picker = state.get_current_picker(prompt_buffer_number)
+
+            -- This is the line you need to remove the entry
+            require("harpoon"):list():remove(selected_entry)
+            current_picker:refresh(make_finder())
+          end)
+
+          return true
+        end,
       })
       :find()
   end
