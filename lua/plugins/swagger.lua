f330e2a2210d46cfda06e897d3487064e3fb9f73
Noah Bauer <accounts@noahbauer.de>
2024-11-05 00:40:32 +0100

    [ADD] swagger ui wathcer plugin

---
+++ lua/plugins/swagger.lua
@@ -0,0 +1,6 @@
+return {
+  "vinnymeller/swagger-preview.nvim",
+  cmd = { "SwaggerPreview", "SwaggerPreviewStop", "SwaggerPreviewToggle" },
+  build = "npm install -g swagger-ui-watcher && npm i",
+  config = true,
+}
