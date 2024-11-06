828fc12e949a171df3c8bbcd94150bfcf040991b
Noah Bauer <accounts@noahbauer.de>
2024-11-06 03:02:43 +0100

    [REFAC] set foldcolumn to 1

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -203,7 +203,7 @@ return {
     },
     init = function()
       vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-      vim.o.foldcolumn = "auto:2" -- '0' is not bad
+      vim.o.foldcolumn = "1" -- '0' is not bad
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
