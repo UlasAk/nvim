d58b0d8a709babc8b10e893bf33a37dffa271de2
NorinB <accounts@noahbauer.de>
2024-11-19 13:01:26 +0100

    [REFAC] make dashboard more compact

--- lua/configs/snacks.lua
+++ lua/configs/snacks.lua
@@ -134,9 +134,9 @@ M.notifier = {
 M.dashboard = {
   sections = {
     { section = "header" },
-    { section = "keys", gap = 1 },
-    { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = { 2, 2 } },
-    { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+    { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+    { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+    { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
     { section = "startup" },
   },
 }
