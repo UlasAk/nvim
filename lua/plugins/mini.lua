a17365a98b2ff750b1ea7eb68b1f3842681460ca
Noah Bauer <accounts@noahbauer.de>
2024-11-27 00:07:35 +0100

    [ADD] plugins: mini.move and disable mini animate scroll

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,7 +10,73 @@ return {
     "echasnovski/mini.animate",
     version = false,
     config = function()
-      require("mini.animate").setup()
+      require("mini.animate").setup {
+        -- Cursor path
+        cursor = {
+          -- Whether to enable this animation
+          enable = true,
+
+          -- Timing of animation (how steps will progress in time)
+          -- timing = --<function: implements linear total 250ms animation duration>,
+
+          -- Path generator for visualized cursor movement
+          -- path = --<function: implements shortest line path>,
+        },
+
+        -- Vertical scroll
+        scroll = {
+          -- Whether to enable this animation
+          enable = false,
+
+          -- Timing of animation (how steps will progress in time)
+          -- timing = --<function: implements linear total 250ms animation duration>,
+
+          -- Subscroll generator based on total scroll
+          -- subscroll = --<function: implements equal scroll with at most 60 steps>,
+        },
+
+        -- Window resize
+        resize = {
+          -- Whether to enable this animation
+          enable = true,
+
+          -- Timing of animation (how steps will progress in time)
+          -- timing = --<function: implements linear total 250ms animation duration>,
+
+          -- Subresize generator for all steps of resize animations
+          -- subresize = --<function: implements equal linear steps>,
+        },
+
+        -- Window open
+        open = {
+          -- Whether to enable this animation
+          enable = true,
+
+          -- Timing of animation (how steps will progress in time)
+          -- timing = --<function: implements linear total 250ms animation duration>,
+
+          -- Floating window config generator visualizing specific window
+          -- winconfig = --<function: implements static window for 25 steps>,
+
+          -- 'winblend' (window transparency) generator for floating window
+          -- winblend = --<function: implements equal linear steps from 80 to 100>,
+        },
+
+        -- Window close
+        close = {
+          -- Whether to enable this animation
+          enable = true,
+
+          -- Timing of animation (how steps will progress in time)
+          -- timing = --<function: implements linear total 250ms animation duration>,
+
+          -- Floating window config generator visualizing specific window
+          -- winconfig = --<function: implements static window for 25 steps>,
+
+          -- 'winblend' (window transparency) generator for floating window
+          -- winblend = --<function: implements equal linear steps from 80 to 100>,
+        },
+      }
     end,
   },
   {
@@ -46,6 +112,34 @@ return {
       require("mini.misc").setup {
         -- Array of fields to make global (to be used as independent variables)
         make_global = { "put", "put_text" },
+      }
+    end,
+  },
+  {
+    "echasnovski/mini.move",
+    version = false,
+    config = function()
+      require("mini.move").setup {
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+          -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+          left = "<M-h>",
+          right = "<M-l>",
+          down = "<M-j>",
+          up = "<M-k>",
+
+          -- Move current line in Normal mode
+          line_left = "<M-h>",
+          line_right = "<M-l>",
+          line_down = "<M-j>",
+          line_up = "<M-k>",
+        },
+
+        -- Options which control moving behavior
+        options = {
+          -- Automatically reindent selection during linewise vertical move
+          reindent_linewise = true,
+        },
       }
     end,
   },
