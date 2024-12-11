3c0231fd9a52fd2fa34de4ab78d1bf1c7ea8e788
NorinB <accounts@noahbauer.de>
2024-12-11 14:55:14 +0100

    [REFAC] lsp_lines: toggle off lsp_lines on startup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -89,10 +89,9 @@ return {
   {
     "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     config = function()
-      require("lsp_lines").setup()
-      vim.diagnostic.config {
-        virtual_text = false,
-      }
+      local lsp_lines = require "lsp_lines"
+      lsp_lines.setup()
+      lsp_lines.toggle()
     end,
   },
 }
