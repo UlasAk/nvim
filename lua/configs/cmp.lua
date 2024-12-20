be60d27ed6d5405787e2e350b78d268449ef2b2b
NorinB <accounts@noahbauer.de>
2024-12-20 09:55:36 +0100

    [REFAC] cmp: use nvim-cmp instead of blink for now

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -69,49 +69,49 @@ local options = {
   formatting = formatting_style,
 
   mapping = {
-    -- ["<C-p>"] = cmp.mapping.select_prev_item(),
-    -- ["<C-n>"] = cmp.mapping.select_next_item(),
-    -- ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    -- ["<C-Space>"] = cmp.mapping.complete(),
-    -- ["<C-e>"] = cmp.mapping.close(),
-    --
-    -- ["<CR>"] = cmp.mapping.confirm {
-    --   behavior = cmp.ConfirmBehavior.Insert,
-    --   select = false,
-    -- },
-    --
-    -- ["<Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   elseif require("luasnip").expand_or_jumpable() then
-    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-    --   else
-    --     fallback()
-    --   end
-    -- end, { "i", "s" }),
-    --
-    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   elseif require("luasnip").jumpable(-1) then
-    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-    --   else
-    --     fallback()
-    --   end
-    -- end, { "i", "s" }),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = false,
+    },
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif require("luasnip").expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif require("luasnip").jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   },
   sources = {
-    -- { name = "nvim_lsp" },
-    -- { name = "luasnip" },
-    -- { name = "buffer" },
-    -- { name = "nvim_lua" },
-    -- { name = "path" },
-    -- { name = "crates" },
-    -- {
-    --   name = "lazydev",
-    --   group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-    -- },
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "nvim_lua" },
+    { name = "path" },
+    { name = "crates" },
+    {
+      name = "lazydev",
+      group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+    },
   },
 }
 
