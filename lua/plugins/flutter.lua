8fc7b0edfa678232dc20964c7000fa65ac859c31
NorinB <accounts@noahbauer.de>
2025-01-28 21:39:27 +0100

    [REFAC] flutter-tools: disable foreground color highlighting

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -147,7 +147,7 @@ return {
             enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
             background = true, -- highlight the background
             background_color = { r = 19, g = 17, b = 24 }, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
-            foreground = true, -- highlight the foreground
+            foreground = false, -- highlight the foreground
             virtual_text = false, -- show the highlight using virtual text
             virtual_text_str = "■", -- the virtual text character to highlight
           },
