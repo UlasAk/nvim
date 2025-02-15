22039e424f8b211d814452b0b81747705dd26537
Noah Bauer <bauer.noah712@gmx.de>
2025-02-16 00:51:23 +0100

    [FIX] flash/surround: fix conflicting visual keybinding

--- lua/plugins/jump.lua
+++ lua/plugins/jump.lua
@@ -7,7 +7,7 @@ return {
     -- stylua: ignore
     keys = {
       { "m", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "M", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Flash Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Flash Treesitter Search" },
       { "<c-S>", mode = { "c" }, function() require("flash").toggle() end, desc = "Flash Toggle Flash Search" },
