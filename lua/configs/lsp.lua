8e4de87907f09af5295a33218f2d48ca21bbd991
NorinB <accounts@noahbauer.de>
2025-02-19 16:12:41 +0100

    [ADD] lsp config: add go to definition vsplit keybind

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -133,6 +133,82 @@ local make_capabilities = function()
   return capabilities
 end
 
+local function is_terminal_window(win)
+  local buf = vim.api.nvim_win_get_buf(win)
+  return vim.api.nvim_get_option_value("buftype", { buf = buf }) == "terminal"
+end
+
+local function count_terminal_buffers_in_tabpage(windows)
+  local terminal_count = 0
+
+  for _, win in ipairs(windows) do
+    if is_terminal_window(win) then
+      terminal_count = terminal_count + 1
+    end
+  end
+
+  return terminal_count
+end
+
+local gotoDefinitionInSplit = function()
+  -- Check how many windows are open
+  local wins = vim.api.nvim_tabpage_list_wins(0)
+  local first_buffer = vim.api.nvim_win_get_buf(wins[1])
+
+  local nvim_tree_open = vim.api.nvim_get_option_value("filetype", { buf = first_buffer }) == "NvimTree"
+
+  local current_cursor_pos = vim.api.nvim_win_get_cursor(0)
+
+  local current_buf = vim.api.nvim_get_current_buf()
+  local current_win = vim.api.nvim_get_current_win()
+
+  local target_win
+
+  local relevant_win_count = #wins - count_terminal_buffers_in_tabpage(wins)
+
+  -- If there are already splits, then take the next one and set buffer to current buffer
+  if nvim_tree_open then
+    if relevant_win_count == 1 then
+      print "Cannot open definition from NvimTree window"
+    -- only nvim and one window is open
+    elseif relevant_win_count == 2 then
+      vim.cmd "vsplit"
+      target_win = current_win
+    else
+      for _, win_num in pairs(wins) do
+        if win_num ~= current_win and win_num ~= wins[1] and not is_terminal_window(win_num) then
+          target_win = win_num
+          break
+        end
+      end
+    end
+  else
+    if relevant_win_count >= 2 then
+      for _, win_num in pairs(wins) do
+        if win_num ~= current_win and not is_terminal_window(win_num) then
+          target_win = win_num
+          break
+        end
+      end
+    else
+      vim.cmd "vsplit"
+      target_win = current_win
+    end
+  end
+
+  -- Set buffer for new window
+  vim.api.nvim_win_set_buf(target_win, current_buf)
+
+  -- Copy cursor position to new window for lsp defintion
+  vim.api.nvim_win_set_cursor(target_win, current_cursor_pos)
+
+  -- Focus new window
+  vim.api.nvim_set_current_win(target_win)
+
+  -- Call lsp
+  vim.lsp.buf.definition()
+end
+
 M.capabilities = make_capabilities()
 M.on_init = function(client, _)
   if client.supports_method "textDocument/semanticTokens" then
@@ -148,6 +224,9 @@ M.setup_keymaps = function()
   map("n", "<leader>lgD", vim.lsp.buf.declaration, opts "Lsp Go to declaration")
   map("n", "<leader>lgd", function()
     require("telescope.builtin").lsp_definitions()
+  end, opts "Lsp Go to definition")
+  map("n", "<leader>lgvd", function()
+    gotoDefinitionInSplit()
   end, opts "Lsp Go to definition")
   map("n", "<leader>lh", vim.lsp.buf.hover, opts "Lsp hover information")
   map("n", "<leader>lgi", function()
