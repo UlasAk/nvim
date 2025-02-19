926646c6f2d643a126a35eeb32010aabf60bd3f3
NorinB <accounts@noahbauer.de>
2025-02-19 13:51:57 +0100

    [REFAC] neotest: add keybinds and change config to opts

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -13,9 +13,52 @@ return {
     },
     cmd = "Neotest summary",
     event = { "BufEnter *spec*", "BufEnter *test*" },
-    config = function()
-      local opts = require "configs.neotest"
-      require("neotest").setup(opts)
+    keys = {
+      {
+        "<leader>Td",
+        function()
+          require("neotest").run.run { strategy = "dap" }
+        end,
+        desc = "Test Debug nearest",
+      },
+      {
+        "<leader>Tr",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "Test Run nearest",
+      },
+      {
+        "<leader>TR",
+        function()
+          require("neotest").run.run(vim.fn.expand "%")
+        end,
+        desc = "Test Run file",
+      },
+      {
+        "<leader>TD",
+        function()
+          require("neotest").run.run { vim.fn.expand "%", strategy = "dap" }
+        end,
+        desc = "Test Run file",
+      },
+      {
+        "<leader>Ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "Test Toggle summary",
+      },
+      {
+        "<leader>To",
+        function()
+          require("neotest").output_panel.toggle()
+        end,
+        desc = "Test Toggle output",
+      },
+    },
+    opts = function()
+      return require "configs.neotest"
     end,
   },
 }
