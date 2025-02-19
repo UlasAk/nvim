718f8d1e381b5db5f3908ce67d0ecd4f5965ccee
NorinB <accounts@noahbauer.de>
2025-02-19 13:58:41 +0100

    [REFAC] flutter: add FlutterRun keybind

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -22,6 +22,7 @@ return {
         desc = "Telescope Flutter commands",
       },
       { "<leader>fld", "<cmd>FlutterDevices<CR>", desc = "Flutter Select Device" },
+      { "<leader>flr", "<cmd>FlutterRun<CR>", desc = "Flutter Run" },
     },
     opts = function()
       local lspconfig = require "configs.lsp"
