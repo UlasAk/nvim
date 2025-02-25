42e6cde5ae86534026c39abc00ca16e81e77994f
NorinB <accounts@noahbauer.de>
2025-02-25 19:02:52 +0100

    [ADD] plugins: vim-flog and font

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 - node/npm
 - ripgrep
 - yazi
+
+## Fonts
+Install the fonts from the `fonts/` directory to get nice symbols for vim-flog
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -84,4 +84,12 @@ return {
       },
     },
   },
+  {
+    "rbong/vim-flog",
+    lazy = true,
+    cmd = { "Flog", "Flogsplit", "Floggit" },
+    dependencies = {
+      "tpope/vim-fugitive",
+    },
+  },
 }
