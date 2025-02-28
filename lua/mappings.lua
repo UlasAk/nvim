a06de6c9b7a6213c98508386deb941a3e0772e70
NorinB <accounts@noahbauer.de>
2025-02-28 14:05:18 +0100

    [REFAC] mappings: change wrap binding and add new buffer save mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,11 @@
 local map = vim.keymap.set
 
+-- Buffer
+map("n", "<leader>w", "<cmd>w<CR>", { desc = "Buffer Save" })
+map("n", "<leader>bn", "<cmd>enew<CR>", { desc = "Buffer New" })
+-- map("n", "<C-K>", "<C-y>", { desc = "Buffer Scroll up one line", noremap = true })
+-- map("n", "<C-J>", "<C-e>", { desc = "Buffer Scroll down one line", noremap = true })
+
 -- Jump
 map("i", "<C-b>", "<ESC>^i", { desc = "Jump Beginning of line" })
 map("i", "<C-e>", "<End>", { desc = "Jump End of line" })
@@ -38,7 +44,7 @@ map("n", "<leader>tr", "<cmd>set rnu!<CR>", { desc = "Toggle Relative number" })
 
 -- Wrap
 local wrap = true
-map("n", "<leader>w", function()
+map("n", "<leader>W", function()
   wrap = not wrap
   if wrap then
     vim.cmd "set wrap"
@@ -52,11 +58,6 @@ map("n", "<leader>ldf", vim.diagnostic.open_float, { desc = "Diagnostics Floatin
 map("n", "<leader>ldp", vim.diagnostic.goto_prev, { desc = "Diagnostics Prev diagnostic" })
 map("n", "<leader>ldn", vim.diagnostic.goto_next, { desc = "Diagnostics Next diagnostic" })
 map("n", "<leader>ldl", vim.diagnostic.setloclist, { desc = "Diagnostics Diagnostic loclist" })
-
--- Buffer
--- map("n", "<C-K>", "<C-y>", { desc = "Buffer Scroll up one line", noremap = true })
--- map("n", "<C-J>", "<C-e>", { desc = "Buffer Scroll down one line", noremap = true })
-map("n", "<leader>bn", "<cmd>enew<CR>", { desc = "Buffer New" })
 
 -- Terminal
 map("t", "<C-x>", "<C-\\><C-N>", { desc = "Terminal escape terminal mode" })
