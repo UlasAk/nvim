1f09cc0779dd1302e5e7c2bf76b9e5a519bbc956
NorinB <accounts@noahbauer.de>
2025-03-07 18:28:52 +0100

    [ADD] conform: add specific dart_format config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,12 +1,12 @@
 local M = {}
 
-vim.g.disable_autoformat = true
+vim.g.disable_autoformat = false
 
 M.filetypes = {
   bash = { "shfmt" },
   bib = { "texlab" },
   cs = { "csharpier" },
-  dart = {},
+  dart = { "dart_format" },
   html = { "prettier" },
   htmlangular = { "prettier" },
   javascript = { "prettier" },
@@ -20,6 +20,20 @@ M.filetypes = {
 
 M.options = {
   formatters_by_ft = M.filetypes,
+  formatters = {
+    dart_format = {
+      args = function()
+        local args_table = { "format" }
+        if string.match(vim.fn.expand "%:p", "projects") then
+          local additional_args = { "-l", "120" }
+          for _, arg in pairs(additional_args) do
+            table.insert(args_table, arg)
+          end
+        end
+        return args_table
+      end,
+    },
+  },
   format_on_save = function(bufnr)
     -- Disable with a global or buffer-local variable
     if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
