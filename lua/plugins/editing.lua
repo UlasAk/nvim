dc4c2381f14267d3878f756ddb2c0f749fced9f2
Noah Bauer <accounts@noahbauer.de>
2025-03-23 03:20:36 +0100

    [REFAC] Comment.nvim: improve layz loading and remove unused keys

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -64,14 +64,7 @@ return {
   },
   {
     "numToStr/Comment.nvim",
-    event = { "BufReadPre" },
     keys = {
-      { "gcc", mode = "n", desc = "comment toggle current line" },
-      { "gc", mode = { "n", "o" }, desc = "comment toggle linewise" },
-      { "gc", mode = "x", desc = "comment toggle linewise (visual)" },
-      { "gbc", mode = "n", desc = "comment toggle current block" },
-      { "gb", mode = { "n", "o" }, desc = "comment toggle blockwise" },
-      { "gb", mode = "x", desc = "comment toggle blockwise (visual)" },
       {
         "<leader>/",
         function()
