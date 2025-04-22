a2d57b5acea0164a5023829bd4194246429af7e4
NorinB <accounts@noahbauer.de>
2025-04-22 12:27:22 +0200

    [REFAC] flash: adapt keybindings to not interfere with surround and make m in normal mode available again

--- lua/plugins/globals/jump.lua
+++ lua/plugins/globals/jump.lua
@@ -6,8 +6,10 @@ return {
     opts = {},
     -- stylua: ignore
     keys = {
-      { "m", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "M", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "s", mode = { "n" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "m", mode = { "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "M", mode = { "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Flash Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Flash Treesitter Search" },
       { "<c-S>", mode = { "c" }, function() require("flash").toggle() end, desc = "Flash Toggle Flash Search" },
