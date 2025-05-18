333ec3447065054230e8cd6e1e7e0b4bcc0aad34
Noah Bauer <accounts@noahbauer.de>
2025-05-18 02:27:02 +0200

    [ADD] plugins: incline.nvim

--- lua/plugins/nvim/ui.lua
+++ lua/plugins/nvim/ui.lua
@@ -871,4 +871,36 @@ return {
     },
     opts = {},
   },
+  {
+    "b0o/incline.nvim",
+    event = { "BufReadPost", "BufEnter" },
+    opts = {
+      render = function(props)
+        local helpers = require "incline.helpers"
+        local devicons = require "nvim-web-devicons"
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        if filename == "" then
+          filename = "[No Name]"
+        end
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
+        local modified = vim.bo[props.buf].modified
+        return {
+          ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+          " ",
+          { filename, gui = modified and "bold,italic" or "bold" },
+          " ",
+          guibg = "#44406e",
+        }
+      end,
+      hide = {
+        cursorline = true,
+        focused_win = true,
+      },
+      window = {
+        margin = {
+          vertical = 2,
+        },
+      },
+    },
+  },
 }
