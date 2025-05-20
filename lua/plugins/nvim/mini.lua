268aac04324bfb4b33e87508c549c278634891a8
NorinB <accounts@noahbauer.de>
2025-05-20 22:35:51 +0200

    [REMOVE] plugins: mini.misc since := exists

--- lua/plugins/nvim/mini.lua
+++ lua/plugins/nvim/mini.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "echasnovski/mini.misc",
-    version = false,
-    priority = 1000,
-    lazy = false,
-    opts = {
-      -- Array of fields to make global (to be used as independent variables)
-      make_global = { "put", "put_text" },
-    },
-  },
-  {
     "echasnovski/mini.files",
     version = false,
     keys = {
