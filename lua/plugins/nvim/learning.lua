6fb66b66d87d740c89f5f1dfe03dfe62656e1f6b
NorinB <accounts@noahbauer.de>
2025-07-10 12:54:04 +0200

    [REMOVE] hardtime: commented out, because currently not used

--- lua/plugins/nvim/learning.lua
+++ lua/plugins/nvim/learning.lua
@@ -1,22 +1,22 @@
 return {
-  {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    keys = {
-      {
-        "<leader>th",
-        function()
-          local hardtime = require "hardtime"
-          hardtime.toggle()
-          Snacks.notify(hardtime.is_plugin_enabled and "Enabled" or "Disabled", { title = "Hardtime" })
-        end,
-        desc = "Toggle Hardtime",
-      },
-    },
-    opts = {
-      disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
-    },
-  },
+  -- {
+  --   "m4xshen/hardtime.nvim",
+  --   dependencies = { "MunifTanjim/nui.nvim" },
+  --   keys = {
+  --     {
+  --       "<leader>th",
+  --       function()
+  --         local hardtime = require "hardtime"
+  --         hardtime.toggle()
+  --         Snacks.notify(hardtime.is_plugin_enabled and "Enabled" or "Disabled", { title = "Hardtime" })
+  --       end,
+  --       desc = "Toggle Hardtime",
+  --     },
+  --   },
+  --   opts = {
+  --     disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+  --   },
+  -- },
   {
     "tris203/precognition.nvim",
     keys = {
