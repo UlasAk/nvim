e26e8cc59400ba40d36072a76e82b86490be9469
NorinB <accounts@noahbauer.de>
2025-07-10 12:54:43 +0200

    [REMOVE] precognition: commented out, because currently not used

--- lua/plugins/nvim/learning.lua
+++ lua/plugins/nvim/learning.lua
@@ -17,49 +17,49 @@ return {
   --     disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
   --   },
   -- },
-  {
-    "tris203/precognition.nvim",
-    keys = {
-      {
-        "<leader>tp",
-        function()
-          Snacks.notify(require("precognition").toggle() and "Enabled" or "Disabled", { title = "Precognition" })
-        end,
-        desc = "Toggle Precognition",
-      },
-      {
-        "<leader>pp",
-        function()
-          require("precognition").peek()
-        end,
-        desc = "Toggle Precognition",
-      },
-    },
-    opts = {
-      startVisible = false,
-      -- showBlankVirtLine = true,
-      -- highlightColor = { link = "Comment" },
-      -- hints = {
-      --      Caret = { text = "^", prio = 2 },
-      --      Dollar = { text = "$", prio = 1 },
-      --      MatchingPair = { text = "%", prio = 5 },
-      --      Zero = { text = "0", prio = 1 },
-      --      w = { text = "w", prio = 10 },
-      --      b = { text = "b", prio = 9 },
-      --      e = { text = "e", prio = 8 },
-      --      W = { text = "W", prio = 7 },
-      --      B = { text = "B", prio = 6 },
-      --      E = { text = "E", prio = 5 },
-      -- },
-      -- gutterHints = {
-      --     G = { text = "G", prio = 10 },
-      --     gg = { text = "gg", prio = 9 },
-      --     PrevParagraph = { text = "{", prio = 8 },
-      --     NextParagraph = { text = "}", prio = 8 },
-      -- },
-      -- disabled_fts = {
-      --     "startify",
-      -- },
-    },
-  },
+  -- {
+  --   "tris203/precognition.nvim",
+  --   keys = {
+  --     {
+  --       "<leader>tp",
+  --       function()
+  --         Snacks.notify(require("precognition").toggle() and "Enabled" or "Disabled", { title = "Precognition" })
+  --       end,
+  --       desc = "Toggle Precognition",
+  --     },
+  --     {
+  --       "<leader>pp",
+  --       function()
+  --         require("precognition").peek()
+  --       end,
+  --       desc = "Toggle Precognition",
+  --     },
+  --   },
+  --   opts = {
+  --     startVisible = false,
+  --     -- showBlankVirtLine = true,
+  --     -- highlightColor = { link = "Comment" },
+  --     -- hints = {
+  --     --      Caret = { text = "^", prio = 2 },
+  --     --      Dollar = { text = "$", prio = 1 },
+  --     --      MatchingPair = { text = "%", prio = 5 },
+  --     --      Zero = { text = "0", prio = 1 },
+  --     --      w = { text = "w", prio = 10 },
+  --     --      b = { text = "b", prio = 9 },
+  --     --      e = { text = "e", prio = 8 },
+  --     --      W = { text = "W", prio = 7 },
+  --     --      B = { text = "B", prio = 6 },
+  --     --      E = { text = "E", prio = 5 },
+  --     -- },
+  --     -- gutterHints = {
+  --     --     G = { text = "G", prio = 10 },
+  --     --     gg = { text = "gg", prio = 9 },
+  --     --     PrevParagraph = { text = "{", prio = 8 },
+  --     --     NextParagraph = { text = "}", prio = 8 },
+  --     -- },
+  --     -- disabled_fts = {
+  --     --     "startify",
+  --     -- },
+  --   },
+  -- },
 }
