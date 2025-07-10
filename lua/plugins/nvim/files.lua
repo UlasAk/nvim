808564c6c2ef9e50023f39f79fbbf0877d6bd8f5
NorinB <accounts@noahbauer.de>
2025-07-10 12:43:31 +0200

    [REMOVE] nvim-spectre

--- lua/plugins/nvim/files.lua
+++ lua/plugins/nvim/files.lua
@@ -340,41 +340,6 @@ return {
     },
   },
   {
-    "nvim-pack/nvim-spectre",
-    cmd = "Spectre",
-    keys = {
-      {
-        "<leader>S",
-        function()
-          require("spectre").toggle()
-        end,
-        desc = "Spectre Toggle",
-      },
-      {
-        "<leader>sw",
-        function()
-          require("spectre").open_visual { select_word = true }
-        end,
-        desc = "Spectre Search current word",
-      },
-      {
-        "<leader>sw",
-        function()
-          require("spectre").open_visual {}
-        end,
-        mode = "v",
-        desc = "Spectre Search current word",
-      },
-      {
-        "<leader>sof",
-        function()
-          require("spectre").open_file_search { select_word = true }
-        end,
-        desc = "Spectre Search on current file",
-      },
-    },
-  },
-  {
     "MagicDuck/grug-far.nvim",
     cmd = "GrugFar",
     keys = {
