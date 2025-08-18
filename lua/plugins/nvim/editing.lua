75a8d2dc567e7162fc27d761beae42d3d9aecf32
NorinB <accounts@noahbauer.de>
2025-08-18 16:08:15 +0200

    [ADD] plugins: undotree

--- lua/plugins/nvim/editing.lua
+++ lua/plugins/nvim/editing.lua
@@ -114,6 +114,14 @@ return {
       }
     end,
   },
+  {
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+    opts = {},
+  },
   -- {
   --   "kevinhwang91/nvim-ufo",
   --   dependencies = {
