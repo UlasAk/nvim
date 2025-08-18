6144a3b34da68fd8b765f8a913d9d26d22719be2
NorinB <accounts@noahbauer.de>
2025-08-18 16:08:06 +0200

    [REMOVE] plugins: telescope-undo

--- lua/plugins/nvim/telescope.lua
+++ lua/plugins/nvim/telescope.lua
@@ -317,37 +317,37 @@ return {
       telescope.load_extension "smart_open"
     end,
   },
-  {
-    "debugloop/telescope-undo.nvim",
-    dependencies = { -- note how they're inverted to above example
-      {
-        "nvim-telescope/telescope.nvim",
-      },
-    },
-    keys = {
-      {
-        "<leader>u",
-        "<cmd>Telescope undo<cr>",
-        desc = "Telescope Undo history",
-      },
-    },
-    opts = {
-      -- don't use `defaults = { }` here, do this in the main telescope spec
-      extensions = {
-        undo = {
-          -- telescope-undo.nvim config, see below
-        },
-        -- no other extensions here, they can have their own spec too
-      },
-    },
-    config = function(_, opts)
-      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
-      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
-      -- defaults, as well as each extension).
-      require("telescope").setup(opts)
-      require("telescope").load_extension "undo"
-    end,
-  },
+  -- {
+  --   "debugloop/telescope-undo.nvim",
+  --   dependencies = { -- note how they're inverted to above example
+  --     {
+  --       "nvim-telescope/telescope.nvim",
+  --     },
+  --   },
+  --   keys = {
+  --     {
+  --       "<leader>u",
+  --       "<cmd>Telescope undo<cr>",
+  --       desc = "Telescope Undo history",
+  --     },
+  --   },
+  --   opts = {
+  --     -- don't use `defaults = { }` here, do this in the main telescope spec
+  --     extensions = {
+  --       undo = {
+  --         -- telescope-undo.nvim config, see below
+  --       },
+  --       -- no other extensions here, they can have their own spec too
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+  --     -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+  --     -- defaults, as well as each extension).
+  --     require("telescope").setup(opts)
+  --     require("telescope").load_extension "undo"
+  --   end,
+  -- },
   {
     "nvim-telescope/telescope-dap.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap" },
