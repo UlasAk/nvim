938147751b0558b5fae533af961b91618c9226e9
NorinB <accounts@noahbauer.de>
2025-08-19 14:58:11 +0200

    [REFAC] undotree: add keybind description

--- lua/plugins/nvim/editing.lua
+++ lua/plugins/nvim/editing.lua
@@ -118,7 +118,7 @@ return {
     "jiaoshijie/undotree",
     dependencies = "nvim-lua/plenary.nvim",
     keys = { -- load the plugin only when using it's keybinding:
-      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", desc = "Toggle Undotree" },
     },
     opts = {},
   },
