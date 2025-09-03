abe1245c65fdb5092370314397c45e55603c3142
NorinB <accounts@noahbauer.de>
2025-09-03 15:05:36 +0200

    [REFAC] flutter-tools: set dart lsp option 'onlyAnalyzeProjectsWithOpenFiles' to false

--- lua/plugins/nvim/flutter.lua
+++ lua/plugins/nvim/flutter.lua
@@ -145,7 +145,7 @@ return {
             renameFilesWithClasses = "prompt", -- "always"
             enableSnippets = true,
             updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
-            onlyAnalyzeProjectsWithOpenFiles = true,
+            onlyAnalyzeProjectsWithOpenFiles = false,
           },
         },
       }
