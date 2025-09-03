acc0a5ac01eb41f3d2b46a13826fe4c44e4fda1d
NorinB <accounts@noahbauer.de>
2025-09-03 08:29:26 +0200

    [REMOVE] mappings: remove tab navigate keymaps, since gt and gT exist

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,8 +9,6 @@ map("n", "<leader>bn", "<cmd>enew<CR>", { desc = "Buffer New" })
 -- Tabs
 map("n", "<C-Tab>", "<cmd>tabnext<CR>", { desc = "Tab Next" })
 map("n", "<C-S-Tab>", "<cmd>tabprevious<CR>", { desc = "Tab Previous" })
-map("n", "]t", "<cmd>tabnext<CR>", { desc = "Tab Next" })
-map("n", "[t", "<cmd>tabprevious<CR>", { desc = "Tab Previous" })
 
 -- Jump
 -- map("n", "<C-o>", "<C-i>", { desc = "Jump Forward in Jumplist" })
