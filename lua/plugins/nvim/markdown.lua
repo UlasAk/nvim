b29aff7d62fed8f271df5341fa25065f67b779fc
NorinB <accounts@noahbauer.de>
2025-09-06 20:02:35 +0200

    [REMOVE] plugins: comment out markdown-preview

--- lua/plugins/nvim/markdown.lua
+++ lua/plugins/nvim/markdown.lua
@@ -1,13 +1,13 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-  },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   ft = { "markdown" },
+  --   build = "cd app && npm install",
+  --   init = function()
+  --     vim.g.mkdp_filetypes = { "markdown" }
+  --   end,
+  -- },
   {
     "OXY2DEV/markview.nvim",
     -- lazy = false,
