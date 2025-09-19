f7b8c73a77b248895cc247d6e8e0ac6737d716b8
NorinB <accounts@noahbauer.de>
2025-09-19 13:28:54 +0200

    [REFAC] lint: move lint callback directly into the autocmd setup

--- lua/plugins/nvim/format-lint.lua
+++ lua/plugins/nvim/format-lint.lua
@@ -108,18 +108,16 @@ return {
       local mason_config = require "mason-opts"
       lint.linters_by_ft = mason_config.get_filetype_linter_nvim_lint_map()
 
-      local callback = function()
-        if vim.opt_local.modifiable:get() then
-          lint.try_lint()
-        end
-      end
-
       vim.schedule(function()
         vim.diagnostic.config { virtual_text = false }
       end)
 
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        callback = callback,
+        callback = function()
+          if vim.opt_local.modifiable:get() then
+            lint.try_lint()
+          end
+        end,
       })
     end,
   },
