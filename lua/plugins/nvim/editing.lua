d1cc4320c134fc4f2d8a70a0be85e6e04ef91f7e
NorinB <accounts@noahbauer.de>
2025-09-25 01:59:47 +0200

    [REMOVE] nvim-autopairs: remove nvim-cmp from dependencies, since it is not used anymore

--- lua/plugins/nvim/editing.lua
+++ lua/plugins/nvim/editing.lua
@@ -1,19 +1,11 @@
 return {
   {
     "windwp/nvim-autopairs",
-    dependencies = { "hrsh7th/nvim-cmp" },
     event = "InsertEnter",
     opts = {
       fast_wrap = {},
       disable_filetype = { "TelescopePrompt", "vim" },
     },
-    config = function(_, opts)
-      require("nvim-autopairs").setup(opts)
-
-      -- setup cmp for autopairs
-      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-      require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
   },
   {
     "lukas-reineke/indent-blankline.nvim",
