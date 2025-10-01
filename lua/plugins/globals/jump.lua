069342cfa979f7a82938b03d733a063f27666263
NorinB <accounts@noahbauer.de>
2025-10-01 23:39:39 +0200

    [REFAC] flash: disable char flash

--- lua/plugins/globals/jump.lua
+++ lua/plugins/globals/jump.lua
@@ -3,16 +3,22 @@ return {
     "folke/flash.nvim",
     event = "BufReadPost",
     ---@type Flash.Config
-    opts = {},
+    opts = {
+      modes = {
+        char = {
+          enabled = false,
+        },
+      },
+    },
     -- stylua: ignore
     keys = {
       { "s", mode = { "n" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "n", mode = { "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "N", mode = { "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Flash Remote Flash" },
+      { "r", mode = { "o", "x" }, function() require("flash").remote() end, desc = "Flash Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Flash Treesitter Search" },
-      { "<c-S>", mode = { "c" }, function() require("flash").toggle() end, desc = "Flash Toggle Flash Search" },
+      { "<c-f>", mode = { "c" }, function() require("flash").toggle() end, desc = "Flash Toggle Flash Search" },
     },
   },
 }
