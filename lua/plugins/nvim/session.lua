df25b3c7e87800ae4b76c0036a816d7764903468
NorinB <accounts@noahbauer.de>
2025-10-02 22:32:47 +0200

    [REFAC] auto-session: disable auto restore and add session restore keymap

--- lua/plugins/nvim/session.lua
+++ lua/plugins/nvim/session.lua
@@ -5,11 +5,13 @@ return {
     keys = {
       { "<leader>fse", "<cmd>AutoSession search<CR>", desc = "Telescope Sessions" },
       { "<leader>sd", "<cmd>AutoSession delete<CR>", desc = "Session Delete current session" },
+      { "<leader>sl", "<cmd>AutoSession restore<CR>", desc = "Session Restore last in cwd" },
     },
     ---enables autocomplete for opts
     ---@module "auto-session"
     ---@type AutoSession.Config
     opts = {
+      auto_restore = false,
       suppressed_dirs = { "~/", "~/Downloads", "/" },
       cwd_change_handling = true,
       auto_restore_last_session = false,
