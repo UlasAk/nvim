1f169862362d091eca50ecfb3e6022c5fb787494
NorinB <accounts@noahbauer.de>
2025-10-03 04:32:18 +0200

    [REMOVE] move: remove comments

--- lua/plugins/globals/move.lua
+++ lua/plugins/globals/move.lua
@@ -4,24 +4,19 @@ return {
     version = false,
     event = "BufReadPost",
     opts = {
-      -- Module mappings. Use `''` (empty string) to disable one.
       mappings = {
-        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+        -- Visual Mode
         left = "<M-h>",
         right = "<M-l>",
         down = "<M-j>",
         up = "<M-k>",
-
-        -- Move current line in Normal mode
+        -- Normal Mode
         line_left = "<M-h>",
         line_right = "<M-l>",
         line_down = "<M-j>",
         line_up = "<M-k>",
       },
-
-      -- Options which control moving behavior
       options = {
-        -- Automatically reindent selection during linewise vertical move
         reindent_linewise = true,
       },
     },
