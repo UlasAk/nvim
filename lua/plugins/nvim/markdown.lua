7d40fd3b0b9140d8192cce159bd41108956e1c49
NorinB <accounts@noahbauer.de>
2025-10-03 05:22:13 +0200

    [REMOVE] markdown: comments and commented out plugins

--- lua/plugins/nvim/markdown.lua
+++ lua/plugins/nvim/markdown.lua
@@ -1,13 +1,4 @@
 return {
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --   ft = { "markdown" },
-  --   build = "cd app && npm install",
-  --   init = function()
-  --     vim.g.mkdp_filetypes = { "markdown" }
-  --   end,
-  -- },
   {
     "OXY2DEV/markview.nvim",
     -- lazy = false,
@@ -28,22 +19,8 @@ return {
     config = function(_, opts)
       require("markview").setup(opts)
       require("markview.extras.checkboxes").setup {
-        --- Default checkbox state(used when adding checkboxes).
-        ---@type string
         default = "X",
-
-        --- Changes how checkboxes are removed.
-        ---@type
-        ---| "disable" Disables the checkbox.
-        ---| "checkbox" Removes the checkbox.
-        ---| "list_item" Removes the list item markers too.
         remove_style = "disable",
-
-        --- Various checkbox states.
-        ---
-        --- States are in sets to quickly change between them
-        --- when there are a lot of states.
-        ---@type string[][]
         states = {
           { " ", "/", "X" },
           { "<", ">" },
