a74a52744716ebe67eccbb3089fd5ae5f1061b23
NorinB <accounts@noahbauer.de>
2025-10-03 15:12:22 +0200

    [REFAC] vim-tmux-navigator: move plugin frmo keybind to jump

--- lua/plugins/nvim/jump.lua
+++ lua/plugins/nvim/jump.lua
@@ -1,5 +1,23 @@
 return {
   {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<C-h>", "<cmd><C-U>TmuxNavigateLeft<CR>", desc = "Window Go to window left (with tmux)" },
+      { "<C-j>", "<cmd><C-U>TmuxNavigateDown<CR>", desc = "Window Go to window down (with tmux)" },
+      { "<C-k>", "<cmd><C-U>TmuxNavigateUp<CR>", desc = "Window Go to window up (with tmux)" },
+      { "<C-l>", "<cmd><C-U>TmuxNavigateRight<CR>", desc = "Window Go to Window right (with tmux)" },
+      { "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>", desc = "Window Go to previous Window (with tmux)" },
+    },
+  },
+  {
     "chentoast/marks.nvim",
     event = "VeryLazy",
     opts = {
