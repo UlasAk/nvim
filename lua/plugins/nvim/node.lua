377fc3ab6f2c6cb33c94e1a643e2ac6370cc274c
NorinB <accounts@noahbauer.de>
2025-10-03 05:23:27 +0200

    [REMOVE] mode: comments

--- lua/plugins/nvim/node.lua
+++ lua/plugins/nvim/node.lua
@@ -14,31 +14,27 @@ return {
     },
     opts = {
       highlights = {
-        up_to_date = { -- highlight for up to date dependency virtual text
+        up_to_date = {
           fg = "#3C4048",
         },
-        outdated = { -- highlight for outdated dependency virtual text
+        outdated = {
           fg = "#d19a66",
         },
-        invalid = { -- highlight for invalid dependency virtual text
+        invalid = {
           fg = "#ee4b2b",
         },
       },
       icons = {
-        enable = true, -- Whether to display icons
+        enable = true,
         style = {
-          up_to_date = "|  ", -- Icon for up to date dependencies
-          outdated = "|  ", -- Icon for outdated dependencies
-          invalid = "|  ", -- Icon for invalid dependencies
+          up_to_date = "|  ",
+          outdated = "|  ",
+          invalid = "|  ",
         },
       },
-      autostart = true, -- Whether to autostart when `package.json` is opened
-      hide_up_to_date = true, -- It hides up to date versions when displaying virtual text
-      hide_unstable_versions = false, -- It hides unstable versions from version list e.g next-11.1.3-canary3
-      -- Can be `npm`, `yarn`, or `pnpm`. Used for `delete`, `install` etc...
-      -- The plugin will try to auto-detect the package manager based on
-      -- `yarn.lock` or `package-lock.json`. If none are found it will use the
-      -- provided one, if nothing is provided it will use `yarn`
+      autostart = true,
+      hide_up_to_date = true,
+      hide_unstable_versions = false,
       package_manager = "npm",
     },
     config = function(_, opts)
@@ -55,25 +51,4 @@ return {
       })
     end,
   },
-  -- {
-  --   "sajjathossain/nvim-npm",
-  --   dependencies = {
-  --     "nvim-telescope/telescope.nvim",
-  --     "akinsho/toggleterm.nvim",
-  --     "rcarriga/nvim-notify",
-  --   },
-  --   cond = function()
-  --     local cwd = vim.fn.getcwd()
-  --     local package_json_path = cwd .. "/package.json"
-  --     if vim.fn.filereadable(package_json_path) == 1 then
-  --       return true
-  --     else
-  --       return false
-  --     end
-  --   end,
-  --   keys = {
-  --     { "<leader>ns", "<cmd>ShowScriptsInTelescope<CR>", desc = "Telescope NPM Scripts" },
-  --   },
-  --   opts = {},
-  -- },
 }
