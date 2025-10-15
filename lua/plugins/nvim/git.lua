1b9ba829530454e94b97dbab7e710b64fb67c23a
NorinB <accounts@noahbauer.de>
2025-10-15 14:53:08 +0200

    [REFAC] git-conflict: cleanup config

--- lua/plugins/nvim/git.lua
+++ lua/plugins/nvim/git.lua
@@ -37,13 +37,6 @@ return {
     },
     opts = {
       default_mappings = false,
-      default_commands = true,
-      disable_diagnostics = false,
-      list_opener = "copen",
-      highlights = {
-        incoming = "DiffAdd",
-        current = "DiffText",
-      },
     },
   },
   {
