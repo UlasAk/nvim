3ce38ceee4103dac60752b99524ac6406e19824d
NorinB <accounts@noahbauer.de>
2025-10-15 16:04:14 +0200

    [REFAC] treesitter-context: cleanup config

--- lua/plugins/nvim/lsp.lua
+++ lua/plugins/nvim/lsp.lua
@@ -357,17 +357,7 @@ return {
       },
     },
     opts = {
-      enable = true,
-      multiwindow = false,
-      max_lines = 0,
-      min_window_height = 0,
-      line_numbers = true,
-      multiline_threshold = 20,
-      trim_scope = "outer",
-      mode = "cursor",
-      separator = nil,
-      zindex = 20,
-      on_attach = nil,
+      multiwindow = true,
     },
     config = function(_, opts)
       require("treesitter-context").setup(opts)
