7fad525f93ad91b5463beb053ee3f134cbd4caad
NorinB <accounts@noahbauer.de>
2025-10-15 14:39:12 +0200

    [REFAC] nvim-tree: cleanup config

--- lua/plugins/nvim/files.lua
+++ lua/plugins/nvim/files.lua
@@ -10,27 +10,21 @@ return {
       { "<leader>e", "<cmd>NvimTreeFocus<CR>", desc = "NvimTree Focus window" },
     },
     opts = {
-      filters = {
-        dotfiles = false,
-      },
-      disable_netrw = false,
       hijack_netrw = false,
       hijack_cursor = true,
-      hijack_unnamed_buffer_when_opening = false,
       sync_root_with_cwd = true,
       update_focused_file = {
         enable = true,
-        update_root = false,
+        update_root = {
+          enable = false,
+        },
       },
       diagnostics = {
         enable = true,
         show_on_dirs = true,
-        show_on_open_dirs = true,
       },
       view = {
         adaptive_size = true,
-        side = "left",
-        width = 30,
         preserve_window_proportions = true,
         relativenumber = true,
       },
@@ -38,53 +32,17 @@ return {
         enable = true,
         ignore = false,
       },
-      filesystem_watchers = {
-        enable = true,
-      },
-      actions = {
-        open_file = {
-          resize_window = true,
-        },
-      },
       renderer = {
-        root_folder_label = false,
-        highlight_git = true,
+        highlight_git = "name",
         highlight_opened_files = "name",
-        highlight_diagnostics = true,
-
+        highlight_diagnostics = "name",
         indent_markers = {
           enable = true,
         },
-
         icons = {
-          show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = true,
-          },
-
           glyphs = {
-            default = "󰈚",
-            symlink = "",
             folder = {
-              default = "",
-              empty = "",
-              empty_open = "",
-              open = "",
-              symlink = "",
-              symlink_open = "",
-              arrow_open = "",
-              arrow_closed = "",
-            },
-            git = {
-              unstaged = "✗",
-              staged = "✓",
-              unmerged = "",
-              renamed = "➜",
-              untracked = "★",
-              deleted = "",
-              ignored = "◌",
+              arrow_closed = "",
             },
           },
         },
