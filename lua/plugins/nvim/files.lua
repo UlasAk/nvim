4df8a61718556406093437fd541ce8b8674be5b7
NorinB <accounts@noahbauer.de>
2025-10-15 13:50:03 +0200

    [REFAC] oil: cleanup config and enalbe preview on startup

--- lua/plugins/nvim/files.lua
+++ lua/plugins/nvim/files.lua
@@ -108,127 +108,53 @@ return {
       {
         "-",
         function()
-          require("oil").open_float()
+          local oil = require "oil"
+          local oil_util = require "oil.util"
+          oil.open_float()
+          oil_util.run_after_load(0, function()
+            oil.open_preview()
+          end)
         end,
         desc = "Oil Open parent directory",
       },
     },
     opts = {
-      default_file_explorer = true,
-      columns = {
-        "icon",
-      },
-      buf_options = {
-        buflisted = false,
-        bufhidden = "hide",
-      },
       win_options = {
         wrap = true,
         signcolumn = "yes:2",
-        cursorcolumn = false,
-        foldcolumn = "0",
-        spell = false,
-        list = false,
-        conceallevel = 3,
-        concealcursor = "nvic",
       },
       delete_to_trash = true,
-      skip_confirm_for_simple_edits = false,
-      prompt_save_on_select_new_entry = true,
-      cleanup_delay_ms = 2000,
-      lsp_file_methods = {
-        enabled = true,
-        timeout_ms = 1000,
-        autosave_changes = false,
-      },
-      constrain_cursor = "editable",
       watch_for_changes = true,
-      keymaps = {
-        ["g?"] = "actions.show_help",
-        ["<CR>"] = "actions.select",
-        ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
-        ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
-        ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
-        ["<C-p>"] = "actions.preview",
-        ["<C-c>"] = "actions.close",
-        ["<C-l>"] = "actions.refresh",
-        ["-"] = "actions.parent",
-        ["_"] = "actions.open_cwd",
-        ["`"] = "actions.cd",
-        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory", mode = "n" },
-        ["gs"] = "actions.change_sort",
-        ["gx"] = "actions.open_external",
-        ["g."] = "actions.toggle_hidden",
-        ["g\\"] = "actions.toggle_trash",
-      },
-      use_default_keymaps = true,
       view_options = {
         show_hidden = true,
-        is_hidden_file = function(name, bufnr)
+        is_hidden_file = function(name)
           return vim.startswith(name, ".")
-        end,
-        is_always_hidden = function(name, bufnr)
-          return false
-        end,
-        natural_order = true,
-        case_insensitive = false,
-        sort = {
-          { "type", "asc" },
-          { "name", "asc" },
-        },
-      },
-      extra_scp_args = {},
-      git = {
-        add = function(path)
-          return false
-        end,
-        mv = function(src_path, dest_path)
-          return false
-        end,
-        rm = function(path)
-          return false
         end,
       },
       float = {
-        padding = 2,
-        max_width = 0,
-        max_height = 0,
         border = "rounded",
         win_options = {
           winblend = 0,
           signcolumn = "yes:2",
         },
-        get_win_title = nil,
         preview_split = "right",
         override = function(conf)
           local new_conf = vim.tbl_deep_extend(
             "force",
             conf,
-            { relative = "editor", anchor = "SW", row = 1000, col = 2, width = 100, height = 50 }
+            { relative = "editor", anchor = "SW", row = 1000, col = 2, width = 200, height = 50 }
           )
           return new_conf
         end,
       },
-      preview = {
-        max_width = 0.9,
-        min_width = { 40, 0.4 },
-        width = nil,
-        max_height = 0.9,
-        min_height = { 5, 0.1 },
-        height = nil,
+      preview_win = {
+        update_on_cursor_moved = true,
         border = "rounded",
         win_options = {
           winblend = 0,
         },
-        update_on_cursor_moved = true,
       },
       progress = {
-        max_width = 0.9,
-        min_width = { 40, 0.4 },
-        width = nil,
-        max_height = { 10, 0.9 },
-        min_height = { 5, 0.1 },
-        height = nil,
         border = "rounded",
         minimized_border = "none",
         win_options = {
